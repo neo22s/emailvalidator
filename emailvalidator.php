6afc72d3956bdb0717c153715d2009f30476cc52
Chema <neo22s@gmail.com>
2023-09-11 22:01:29 +0200

    Update emailvalidator.php
    Now we store it as an array in PHP, we validate the JSON file when reading. 1 month cache. Much faster to read the array using isset.

--- emailvalidator.php
+++ emailvalidator.php
@@ -3,61 +3,88 @@
  * Class email validator
  *
  * @author     Chema <chema@garridodiaz.com>
- * @copyright  (c) 2015
+ * @copyright  (c) 2023
  * @license    GPL v3
+ * @version    2.0
  */
 class emailvalidator
 {
     /**
-     * verify email format, dns and banned emails
+     * Verify email format, DNS, and banned emails
      * @param  string $email 
      * @return mixed   bool true if correct / string
      */
     public static function check($email)
     {
-        //get the email to check up, clean it
-        $email = filter_var($email,FILTER_SANITIZE_STRING);
+        // Get the email to check up, clean it
+        $email = filter_var($email, FILTER_SANITIZE_STRING);
 
-        // 1 - check valid email format using RFC 822
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)===FALSE) 
+        // 1 - Check valid email format using RFC 822
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === FALSE) 
             return 'No valid email format';
             
-        //get email domain to work in nexts checks
+        // Get email domain to work in next checks
         $email_domain = preg_replace('/^[^@]++@/', '', $email);
 
-        // 2 - check if its from banned domains.
-        if (in_array($email_domain,self::get_banned_domains()))
-            return 'Banned domain '.$email_domain;
+        // 2 - Check valid domain name
+        if (filter_var($email_domain, FILTER_VALIDATE_DOMAIN) === FALSE) 
+            return 'No valid domain format';
+
+        // 3 - Check if it's from banned domains.
+        $banned_domains = self::get_banned_domains();
+        if (isset($banned_domains[$email_domain]) AND $banned_domains[$email_domain] !== null) {
+            return 'Banned domain ' . $email_domain;
+        }
               
-        // 3 - check DNS for MX records
-        if ((bool) checkdnsrr($email_domain, 'MX')==FALSE)
+        // 4 - Check DNS for MX records
+        if ((bool) checkdnsrr($email_domain, 'MX') == FALSE)
             return 'DNS MX not found for domain '.$email_domain;
 
-        // 4 - wow actually a real email! congrats ;)
+        // 5 - Wow, actually a real email! Congrats ;)
         return TRUE;
     }
 
     /**
-     * gets the array of not allowed domains for emails, reads from json stores file for 1 week
+     * Gets the array of not allowed domains for emails, reads from JSON stores file for 1 week
      * @return array 
      * @see banned domains https://github.com/ivolo/disposable-email-domains/blob/master/index.json
      * @return array
      */
     private static function get_banned_domains()
     {
-        //where we store the banned domains
-        $file = 'banned_domains.json';
+        $banned_domains = [];
+       
+        $file = plugin_dir_path(self::MAIN_FILE).'banned-domains.php';
 
-        //if the json file is not in local or the file exists but is older than 1 week, regenerate the json
-        if (!file_exists($file) OR (file_exists($file) AND filemtime($file) < strtotime('-1 week')) )
-        {
+        if (!file_exists($file) OR (file_exists($file) AND filemtime($file) < strtotime('-1 month'))) {
+            // If the file doesn't exist or is older than a month, regenerate it
             $banned_domains = file_get_contents("https://rawgit.com/ivolo/disposable-email-domains/master/index.json");
-            if ($banned_domains !== FALSE)
-                file_put_contents($file,$banned_domains,LOCK_EX);
-        }
-        else//get the domains from the file
-            $banned_domains = file_get_contents($file);
 
-        return json_decode($banned_domains);
+            // we could read the CDN file
+            if ($banned_domains !== FALSE) 
+            {
+                $banned_domains = json_decode($banned_domains, true);
+
+                //error reading the JSON file
+                if ($banned_domains === null AND json_last_error() !== JSON_ERROR_NONE) 
+                    return [];
+
+                // Use array_filter with an inline closure function to remove invalid domains, just in case....
+                $banned_domains = array_filter($banned_domains, function ($domain) {
+                    return filter_var($domain, FILTER_VALIDATE_DOMAIN) !== false;
+                }, ARRAY_FILTER_USE_KEY);
+
+              
+                // Store the banned domains as an associative array with domains as keys
+                $banned_domains = array_fill_keys(array_keys(array_flip($banned_domains)), 0);
+
+                file_put_contents($file, '<?php return ' . var_export($banned_domains, true) . ';', LOCK_EX);
+            }
+        }
+        else// Load the domains from the cached PHP file
+            $banned_domains = include($file);
+        
+        return $banned_domains;
     }
+    
 }
